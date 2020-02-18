49263136844efae618156f5c3e1891e776c56848
Mukund@Codeonnet <39686327+mukundkumar83@users.noreply.github.com>
2020-02-18 13:39:54 +0530

    First commit of the code to CodeonDevOps

---
+++ CodeOnDevOpsExample/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CodeOnDevOpsExample.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CodeOnDevOpsExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeOnDevOpsExample.MvcApplication" Language="C#" %>
