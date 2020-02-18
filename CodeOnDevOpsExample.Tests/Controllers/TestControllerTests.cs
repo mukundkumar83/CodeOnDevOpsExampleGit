dbb18dda7bdec91f8ffc9b43f4befc44a5226208
Mukund@Codeonnet <39686327+mukundkumar83@users.noreply.github.com>
2020-02-18 16:40:17 +0530

    Added Failed test  case to test if build is failing or not.

---
+++ CodeOnDevOpsExample.Tests/Controllers/TestControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeOnDevOpsExample.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOnDevOpsExample.Controllers.Tests
+{
+    [TestClass()]
+    public class TestControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            int i = 0;
+            Assert.AreEqual(1, i);
+        }
+    }
+}
